dced5e416f0dcf869d6832e1174b7c3b717f6ca5
Oxel40 <Oxel40@users.noreply.github.com>
2023-02-16 19:08:26 +0100

    add config

---
+++ config/runtime.exs
@@ -0,0 +1,21 @@
+import Config
+
+agent_secret =
+  System.get_env("AGENT_SECRET") ||
+    if config_env() != :prod do
+      "testing"
+    else
+      raise """
+      environment variable AGENT_SECRET is missing.
+      """
+    end
+
+url =
+  if config_env() != :prod do
+    {'localhost', 4000, '/printer-agent/websocket'}
+  else
+    {'flying-petrus.fly.dev', 443, '/printer-agent/websocket'}
+  end
+
+config :petrus_agent, agent_secret: agent_secret
+config :petrus_agent, url: url
